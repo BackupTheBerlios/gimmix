fdde93b745917b674a46fd36c947b6184f225571
priyank <priyank@fbaa97b7-fb20-0410-a00a-febf0785dfd4>
2009-02-06 19:25:20 +0000

    * revert 0.5.6 tag *

--- tags/0.5.6/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-libtoolize -f -c
-aclocal --force
-autoheader -f
-autoconf -f
-cp -f $(dirname $(which automake))/../share/automake/mkinstalldirs ./
-cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
-automake -a -c --gnu --foreign
--- tags/0.5.6/src/gimmix-firstrun.h
+++
@@ -1,7 +0,0 @@
-#ifndef GIMMIX_FIRSTRUN_H
-#define GIMMIX_FIRSTRUN_H
-
-/* Displays the first run dialog */
-void gimmix_show_firstrun_dialog (void);
-
-#endif
